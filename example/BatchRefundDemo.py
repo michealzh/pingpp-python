5fb98058f820aea8c448ebcece846aa8719bbfbd
michealzh <sakurazhang@hotmail.com>
2016-12-28 21:17:01 +0800

    Update BatchRefundDemo.py

--- example/BatchRefundDemo.py
+++ example/BatchRefundDemo.py
@@ -14,7 +14,7 @@ import random
 import os
 
 # api_key 获取方式：登录 [Dashboard](https://dashboard.pingxx.com)->点击管理平台右上角公司名称->开发信息-> Secret Key
-api_key = 'sk_test_ibbTe5jLGCi5rzfH4OqPW9KC'
+pingpp.api_key = 'sk_test_ibbTe5jLGCi5rzfH4OqPW9KC'
 # app_id 获取方式：登录 [Dashboard](https://dashboard.pingxx.com)->点击你创建的应用->应用首页->应用 ID(App ID)
 app_id = 'app_1Gqj58ynP0mHeX1q'
 # 设置 API Key
